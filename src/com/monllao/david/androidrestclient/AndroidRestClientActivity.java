e05ba5ba3e19190144c370c75b6e0d06a62be998
David Monllaó <david.monllao@gmail.com>
2012-04-02 19:50:04 +0200

    Iteration 1 - Android SDK skeleton

---
+++ src/com/monllao/david/androidrestclient/AndroidRestClientActivity.java
@@ -0,0 +1,13 @@
+package com.monllao.david.androidrestclient;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AndroidRestClientActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
