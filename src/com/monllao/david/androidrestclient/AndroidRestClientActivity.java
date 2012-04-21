2a472d7c4c4a327c6a9b1299408a4b48734f5fde
David Monllaó <david.monllao@gmail.com>
2012-04-21 17:48:48 +0200

    Fixing activity lifecycle

--- src/com/monllao/david/androidrestclient/AndroidRestClientActivity.java
+++ src/com/monllao/david/androidrestclient/AndroidRestClientActivity.java
@@ -1,8 +1,13 @@
 package com.monllao.david.androidrestclient;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -12,27 +17,40 @@ import com.monllao.david.androidrestclient.service.GetServerUserService;
 public class AndroidRestClientActivity extends Activity {
     
 	public static String APP_NAME = "AndroidRestClient";
+	protected String serverHost;
+	
 	private GetServerUserReceiver receiver;
 	
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        
-        // Setting the app user 
-        User user = new User(this);
-        
+
+
         // Registering the receiver
         receiver = new GetServerUserReceiver();
         IntentFilter filter = new IntentFilter(Intent.ACTION_SYNC);
         filter.addCategory(Intent.CATEGORY_DEFAULT);
         registerReceiver(receiver, filter);
         
+        // Setting the app user 
+        User user = new User(this);
+        
         // Calling the server to create/retrieve the user
         Intent serverUser = new Intent(this, GetServerUserService.class);
+        serverUser.putExtra("serverHost", serverHost);
         serverUser.putExtra("email", user.getEmail());
         serverUser.putExtra("pwd", user.getPwd());
         startService(serverUser);
     }
+
+    
+    public void onDestroy() {
+    	super.onDestroy();
+
+    	// Unregistering the broadcastreceiver
+    	unregisterReceiver(receiver);
+    }
+    
     
     /**
      * Processes the GetServerUser service return
@@ -43,14 +61,5 @@ public class AndroidRestClientActivity extends Activity {
     }
     
     
-    /**
-     * Called when the app stops
-     */
-    public void onPause() {
-    	super.onPause();
-    	
-    	// Unregistering the broadcastreceiver
-    	unregisterReceiver(receiver);
-    }
     
 }
