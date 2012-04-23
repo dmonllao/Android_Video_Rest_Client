b346a486bde8ad4ee55c1d49bb1020044bb47a21
David Monllaó <david.monllao@gmail.com>
2012-04-23 19:18:56 +0200

    Iteration 1 - GetServerUser updated

--- src/com/monllao/david/androidrestclient/receiver/GetServerUserReceiver.java
+++ src/com/monllao/david/androidrestclient/receiver/GetServerUserReceiver.java
@@ -1,10 +1,15 @@
 package com.monllao.david.androidrestclient.receiver;
 
-import com.monllao.david.androidrestclient.AndroidRestClientActivity;
+import java.io.IOException;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources.NotFoundException;
+import android.util.Log;
+
+import com.monllao.david.androidrestclient.AndroidRestClientActivity;
+import com.monllao.david.androidrestclient.User;
 
 public class GetServerUserReceiver extends BroadcastReceiver {
 
@@ -12,10 +17,22 @@ public class GetServerUserReceiver extends BroadcastReceiver {
 	 * Receives the Service data
 	 */
 	public void onReceive(Context context, Intent intent) {
-		String result = intent.getStringExtra("result");
+
+		User user = (User) intent.getSerializableExtra("user");
 		
+		Log.e(AndroidRestClientActivity.APP_NAME, "GetServerUserReceiver");
+		
+		// Return to the activity to reload user
 		AndroidRestClientActivity activity = (AndroidRestClientActivity)context;
-		activity.processGetServerUser(result);
+		try {
+			activity.processServerUser(user);
+		} catch (NotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
--- src/com/monllao/david/androidrestclient/service/GetServerUserService.java
+++ src/com/monllao/david/androidrestclient/service/GetServerUserService.java
@@ -1,12 +1,28 @@
 package com.monllao.david.androidrestclient.service;
 
-import com.monllao.david.androidrestclient.AndroidRestClientActivity;
-import com.monllao.david.androidrestclient.receiver.GetServerUserReceiver;
+import java.io.IOException;
+import java.util.Map;
+
+import org.restlet.data.Form;
+import org.restlet.engine.Engine;
+import org.restlet.engine.http.connector.HttpClientHelper;
+import org.restlet.ext.jackson.JacksonRepresentation;
+import org.restlet.representation.Representation;
+import org.restlet.resource.ClientResource;
+import org.restlet.resource.ResourceException;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.content.res.Resources.NotFoundException;
 import android.util.Log;
 
+import com.monllao.david.androidrestclient.AndroidRestClientActivity;
+import com.monllao.david.androidrestclient.PropertiesManager;
+import com.monllao.david.androidrestclient.User;
+
+/**
+ * Service to get the user data from her/his user id
+ */
 public class GetServerUserService extends IntentService {
 
 	public GetServerUserService() {
@@ -15,17 +31,42 @@ public class GetServerUserService extends IntentService {
 
 	protected void onHandleIntent(Intent intent) {
 		
-		String email = intent.getStringExtra("email");
-		String pwd = intent.getStringExtra("pwd");
+		// Provided user data (with password)
+		User userdata = (User)intent.getSerializableExtra("user");
+
+		Log.e(AndroidRestClientActivity.APP_NAME, "GetServerUserService");
 		
-		String returned = email + " & " + pwd;
-		Log.e(AndroidRestClientActivity.APP_NAME, "GetServerUserService: " + returned);
+		// requested URL
+		String url = "";
+		try {
+			url = PropertiesManager.get("server.host") + "User/" + userdata.getId();
+		} catch (NotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		// Creating the client resource
+		Engine.getInstance().getRegisteredClients().clear();
+		Engine.getInstance().getRegisteredClients().add(new HttpClientHelper(null));
 		
+		// Send request to the server 
+		ClientResource cr = new ClientResource(url);
+		Representation representation = cr.get();
+
+		Log.e(AndroidRestClientActivity.APP_NAME, "GetServerUserService plain response: " + representation.toString());
+			
+		// Getting the user and loading it into user
+		User user = null;
+		JacksonRepresentation<User> userRepresentation = new JacksonRepresentation<User> (representation, User.class);
+		user = userRepresentation.getObject();
+
+
 		// Give feedback to the activity
-		Intent broadcastIntent = new Intent();
-		broadcastIntent.setAction(Intent.ACTION_SYNC);
-		broadcastIntent.addCategory(Intent.CATEGORY_DEFAULT);
-		broadcastIntent.putExtra("result", returned);
+		Intent broadcastIntent = new Intent(AndroidRestClientActivity.ACTION_GETUSER);
+		broadcastIntent.putExtra("user", user);
 		sendBroadcast(broadcastIntent);
 	}
 
