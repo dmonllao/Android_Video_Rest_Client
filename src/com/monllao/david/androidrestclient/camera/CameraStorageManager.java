44f41e462551c0807bb2f4c5036d5ea77adeb53b
David Monllaó <david.monllao@gmail.com>
2012-04-27 21:45:06 +0200

    Iteration 2 - Adding the camera surfaceview and a files manager

---
+++ src/com/monllao/david/androidrestclient/camera/CameraStorageManager.java
@@ -0,0 +1,39 @@
+package com.monllao.david.androidrestclient.camera;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import com.monllao.david.androidrestclient.AndroidRestClientActivity;
+
+import android.os.Environment;
+import android.util.Log;
+
+public class CameraStorageManager {
+
+    /** Create a File for saving an image or video */
+    public static File getOutputMediaFile() {
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
+
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                  Environment.DIRECTORY_PICTURES), "CameraSimple");
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+
+        // Create the storage directory if it does not exist
+        if (! mediaStorageDir.exists()){
+            if (! mediaStorageDir.mkdirs()){
+                Log.e(AndroidRestClientActivity.APP_NAME, "Failed to create directory " + mediaStorageDir.getAbsolutePath());
+                return null;
+            }
+        }
+
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        mediaFile = new File(mediaStorageDir.getPath() + File.separator + "VID_"+ timeStamp + ".mp4");
+
+        return mediaFile;
+    }
+}
---
+++ src/com/monllao/david/androidrestclient/camera/CameraVideoPreview.java
@@ -0,0 +1,70 @@
+package com.monllao.david.androidrestclient.camera;
+
+import java.io.IOException;
+
+import com.monllao.david.androidrestclient.AndroidRestClientActivity;
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class CameraVideoPreview extends SurfaceView implements SurfaceHolder.Callback {
+
+	private SurfaceHolder mHolder;
+	private Camera mCamera;
+	
+	public CameraVideoPreview(Context context, Camera camera) {
+		super(context);
+		mCamera = camera;
+		
+		mHolder = getHolder();
+		mHolder.addCallback(this);
+		mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+	}
+
+
+	public void surfaceChanged(SurfaceHolder holder, int format, int width,
+			int height) {
+
+		if (mHolder.getSurface() == null) {
+			Log.e(AndroidRestClientActivity.APP_NAME, "Preview surface does not exists");
+			return;
+		}
+
+		// Before making changes
+		try {
+			mCamera.stopPreview();
+		} catch (Exception e) {
+			Log.e(AndroidRestClientActivity.APP_NAME, "Can\'t stop preview");
+		}
+		
+		// TODO; Set preview size, resizable or not, rotate...
+		
+		try {
+			mCamera.setPreviewDisplay(mHolder);
+			mCamera.startPreview();
+		} catch (Exception e) {
+			Log.e(AndroidRestClientActivity.APP_NAME, "Error starting preview " + e.getMessage());
+		}
+	}
+
+	@Override
+	public void surfaceCreated(SurfaceHolder holder) {
+		
+		try {
+			mCamera.setPreviewDisplay(holder);
+			mCamera.startPreview();
+		} catch (IOException e) {
+			Log.e(AndroidRestClientActivity.APP_NAME, "Error setting the camera preview " + e.getMessage());
+		}
+		
+	}
+
+	@Override
+	public void surfaceDestroyed(SurfaceHolder holder) {
+		// Take care of it in the Activity
+	}
+
+}
