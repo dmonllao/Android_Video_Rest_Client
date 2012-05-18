3980d44e473ae1f0128e2d8cbe1af2a22cdff9c7
David Monllaó <david.monllao@gmail.com>
2012-05-18 20:16:15 +0200

    MINOR - Iteration 2 - Identifier name

--- gen/com/monllao/david/androidrestclient/R.java
+++ gen/com/monllao/david/androidrestclient/R.java
@@ -18,7 +18,7 @@ public final class R {
     public static final class id {
         public static final int button_rec=0x7f060001;
         public static final int button_share=0x7f060002;
-        public static final int camera_preview=0x7f060000;
+        public static final int screen=0x7f060000;
         public static final int set_video_data=0x7f060004;
         public static final int title=0x7f060003;
     }
