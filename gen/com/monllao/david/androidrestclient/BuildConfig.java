d1a3d0cd73970694a1be5354f1ce1388acecfd97
David Monllaó <david.monllao@gmail.com>
2012-05-19 13:25:24 +0200

    ENV - Iteration 2 - IDE files modifications to ADT 17 & 18 compliance

---
+++ gen/com/monllao/david/androidrestclient/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.monllao.david.androidrestclient;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
